666b427bf8fc768eea2ca234041ddd68f3d7f481
Neto Costa <costa.neto@unesp.br>
2020-07-10 12:38:33 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <div id="app">
+     <TextField label="First Name" v-model="firstName" :textLimit="15"></TextField>
+     <TextField label="Last Name" v-model="lastName" :textLimit="15"></TextField>
+     
+     <SelectField
+      label="Gender" 
+      v-model="gender" 
+      placeholder="Select your gender"
+      :options="genderList"></SelectField>
+
+      <SelectField
+      label="Age" 
+      v-model="age" 
+      placeholder="Select your age"
+      :options="ageList"></SelectField>
+
+
+     <div>{{fullName}} - {{gender}} - {{age}} years old</div>
+  </div>
+</template>
+
+<script>
+import TextField from "./components/TextField"
+import SelectField from "./components/SelectField"
+
+export default {
+  name: "App",
+  data(){
+    return{
+      firstName: "",
+      lastName: "",
+      gender: "",
+      age: ""
+    }
+  },
+  components: { 
+    TextField,
+    SelectField
+  },
+  computed:{
+    fullName(){
+      return this.firstName + " " + this.lastName;
+    },
+    genderList(){
+      return[
+        {value:"Male", text:"Male"},
+        {value:"Female", text:"Female"},
+        {value:"Other", text:"Other"}
+      ];
+    },
+    ageList(){
+      let result = [];
+      for(let i=16; i<65; i++)
+        result.push({value: i, text: i});
+      return result;
+    }
+  }
+};
+</script>
+
+<style lang="stylus">
+#app
+  margin-top 60px
+
+.text-input
+label 
+  display  block
+
+input 
+  display block
+  width 100%
+
+input.valid
+  border 1px solid green
+
+input.invalid 
+  border 1px solid red
+
+.validation
+  text-align right 
+  font-size 12px
+
+
+</style>
---
+++ src/components/TextField.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="text-input" style="width: 200px;">
+      <div>
+        <label>{{label}}</label>
+        <input :class="validationClass" :value="value" @input="$emit('input', $event.target.value)">
+      </div>
+    
+    <div v-if="textLimit>0" class="validation">{{inputCount}}/{{textLimit}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+      value: {required: true, type: String},
+      textLimit: {required: false, type: Number},
+      label: {required: false, type: String}
+  },
+  watch:{
+      value: function(newValue,oldValue){
+        //console.log(newValue,oldValue);
+        if(newValue.length > this.textLimit){
+          this.$emit('input', oldValue);
+          //this.value=oldValue;
+        }
+      }
+  },
+  computed:{
+    inputCount(){
+      return this.value.length
+    },
+    isEmpty(){
+      return this.value.length === 0
+    },
+    validationClass()
+    {
+      return this.isEmpty ? "invalid" : "valid";
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+  
+</style>
